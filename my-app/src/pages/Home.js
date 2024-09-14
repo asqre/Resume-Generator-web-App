fb7f7b859a3a266954443e176dfeb1b024bb1b94
asqre <amitanand.asqre@gmail.com>
2024-09-15 01:48:44 +0530

    fix padding

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -3,7 +3,7 @@ import ResumeGeneratorForm from "../components/ResumeGeneratorForm";
 
 const Home = () => {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto py-4">
       <h1 className="text-2xl font-bold text-center my-8">Resume Generator</h1>
       <ResumeGeneratorForm />
     </div>
